772f4d1746f62ebbd702c2a9df805530fa37ac69
Eric Chrobak <eric@ericchrobak.com>
2018-05-04 04:06:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |b, a|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a_A = a.length
+    b_B = b.length
+    if a_A == b_B
+      0
+    elsif a_A < b_B
+      -1
+    elsif a_A > b_B
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  first_val = array[1]
+  second_val = array[2]
+  array[1] = second_val
+  array[2] = first_val
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  first_val = array[index]
+  second_val = array[destination_index]
+  array[index] = second_val
+  array[destination_index] = first_val
+  array
+end
+
+def reverse_array(array)
+  i = array.length - 1
+  new_array = []
+  array.collect do |a|
+    new_array[i] = a
+    i -= 1
+  end
+  new_array
+end
